eb22ef5132133212b2188c4fc0d4b18437d38d5b
Tony Arcieri <tony.arcieri@gmail.com>
2013-09-09 23:50:06 -0700

    Basic command line argument support

--- lib/reel/rack/cli.rb
+++ lib/reel/rack/cli.rb
@@ -1,15 +1,49 @@
 require 'reel/rack'
+require 'optparse'
 
 module Reel
   module Rack
     class CLI
       def initialize(argv)
         @argv   = argv
-        @rackup = "config.ru"
+        @options = {
+          addr:   "localhost",
+          port:   3000,
+          quiet:  false,
+          rackup: "config.ru"
+        }
+        parser
+      end
+
+      def parser
+        @parser ||= OptionParser.new do |o|
+          o.banner = "reel-rack <options> <rackup file>"
+
+          o.on "-a", "--addr ADDR", "Address to listen on (default #{@options[:addr]})" do |addr|
+            @options[:addr] = addr
+          end
+
+          o.on "-p", "--port PORT", "Port to bind to (default #{@options[:port]})" do |port|
+            @options[:port] = port
+          end
+
+          o.on "-q", "--quiet", "Suppress normal logging output" do
+            @options[:quiet] = true
+          end
+
+          o.on_tail "-h", "--help", "Show help" do
+            STDOUT.puts @parser
+            exit 1
+          end
+        end
       end
 
       def run
-        app, options = ::Rack::Builder.parse_file(@rackup)
+        @parser.parse! @argv
+        @options[:rackup] = @argv.shift if @argv.last
+
+        app, options = ::Rack::Builder.parse_file(@options[:rackup])
+        options.merge!(@options)
         ::Rack::Handler::Reel.run(app, options)
       end
     end
